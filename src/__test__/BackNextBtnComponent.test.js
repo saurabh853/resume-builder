dfe48387c4ace03d6a41c170df28b627c49242ec
Saurabh Tiwari <saurabhtiwari9825@gmail.com>
2023-12-16 18:20:35 +0530

    liv

--- src/__test__/BackNextBtnComponent.test.js
+++ src/__test__/BackNextBtnComponent.test.js
@@ -4,6 +4,6 @@ import BackNextBtnComponent from '../Components/BackNextBtnComponent';
 
 test('finds elements by class name', () => {
   render(<BackNextBtnComponent />);
-  const backBtn = screen.getByClassName('back-next-btn-cont');
+  const backBtn = screen.getByClassName('back-next-btn-continer');
   expect(backBtn).toBeInTheDocument();
 });
